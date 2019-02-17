6ed077ed658e54c646ad49fedd6bd64b0c2c791f
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2019-02-17 18:47:52 +0000

    added new modifying operations to be compatible with vector; some refactoring

--- tuple_vector.h
+++ tuple_vector.h
@@ -41,13 +41,6 @@ struct key {
  * \author Sinisa Susnjar
  */
 
-/*
-template<typename K = time_t>
-struct key {
-    double operator()(const K &x) const { return x; }
-};
-*/
-
 // declare a custom container based on a vector containing pair<K,V> tuples
 template<typename K, typename V>
 class tuple_vector : public vector<pair<K,V>> {
@@ -64,83 +57,142 @@ public:
 
 	~tuple_vector() { }
 
-	/*
-	inline void fast_add(const K &key, const V &value)
-	{
-		vector<value_type>::emplace_back(make_pair(key, value));
+	// all modifying operations need to set m_recompute_range to true so that find(),
+	// lower_bound() can update internal book-keeping variables
+	inline void assign (const_iterator first, const_iterator last) {
+		vector<value_type>::assign(first, last);
+		m_recompute_range = true;
 	}
-
-	inline void add(const K &key, const V &value)
-	{
-		vector<value_type>::emplace_back(make_pair(key, value));
+	inline void assign (size_type n, const value_type& val) {
+		vector<value_type>::assign(n, val);
+		m_recompute_range = true;
 	}
-
-	inline void add(const std::pair<K, V> &p)
+	inline void assign (initializer_list<value_type> il) {
+		vector<value_type>::assign(il);
+		m_recompute_range = true;
+	}
+	void clear() noexcept {
+		vector<value_type>::clear();
+		m_recompute_range = true;
+	}
+	inline void emplace_back(const value_type &&p)
 	{
 		vector<value_type>::emplace_back(p);
+		m_recompute_range = true;
 	}
-	*/
+	inline iterator emplace(const_iterator pos, value_type &v) {
+		m_recompute_range = true;
+		return vector<value_type>::emplace(pos, v);
+	}
+	inline void emplace_back(const value_type &p)
+	{
+		vector<value_type>::emplace_back(p);
+		m_recompute_range = true;
+	}
+	inline size_type erase(const K &key)
+	{
+		auto pos = find(key);
+		m_recompute_range = true;
+		vector<value_type>::erase(pos);
+		return 1;
+	}
+	inline iterator erase(const_iterator pos)
+	{
+		m_recompute_range = true;
+		return vector<value_type>::erase(pos);
+	}
+	inline iterator erase(const_iterator start, const_iterator end)
+	{
+		m_recompute_range = true;
+		return vector<value_type>::erase(start, end);
+	}
+	inline iterator insert(const_iterator position, const value_type& val) {
+		m_recompute_range = true;
+		return vector<value_type>::insert(position, val);
+	}
+	inline iterator insert(const_iterator position, size_type n, const value_type& val) {
+		m_recompute_range = true;
+		return vector<value_type>::insert(position, n, val);
+	}
+	inline iterator insert(const_iterator position, const_iterator first, const_iterator last) {
+		m_recompute_range = true;
+		return vector<value_type>::insert(position, first, last);
+	}
+	inline iterator insert(const_iterator position, value_type&& val) {
+		m_recompute_range = true;
+		return vector<value_type>::insert(position, val);
+	}
+	inline iterator insert(const_iterator position, initializer_list<value_type> il) {
+		m_recompute_range = true;
+		return vector<value_type>::insert(position, il);
+	}
+	inline tuple_vector<K, V>& operator=(const vector<value_type>& x) {
+		m_recompute_range = true;
+		return vector<value_type>::operator=(x);
+	}
+	inline tuple_vector<K, V>& operator=(vector<value_type>&& x) {
+		m_recompute_range = true;
+		return vector<value_type>::operator=(x);
+	}
+	inline tuple_vector<K, V>& operator=(initializer_list<value_type> il) {
+		m_recompute_range = true;
+		return vector<value_type>::operator=(il);
+	}
+	inline void pop_back() {
+		vector<value_type>::pop_back();
+		m_recompute_range = true;
+	}
+	inline void push_back(const value_type& val) {
+		m_recompute_range = true;
+		vector<value_type>::push_back(val);
+	}
+	inline void push_back(value_type&& val) {
+		m_recompute_range = true;
+		vector<value_type>::push_back(val);
+	}
+	inline void resize(size_type n) {
+		m_recompute_range = true;
+		vector<value_type>::resize(n);
+	}
+	inline void resize(size_type n, const value_type& val) {
+		m_recompute_range = true;
+		vector<value_type>::resize(n, val);
+	}
+	inline void shrink_to_fit() {
+		m_recompute_range = true;
+		vector<value_type>::shrink_to_fit();
+	}
+	inline void swap (vector<value_type> &x) {
+		m_recompute_range = true;
+		vector<value_type>::swap(x);
+	}
 
+	// access operators
+	// map::at()
 	inline iterator at(const K &key)
 	{
 		auto pos = find(key);
 		if (pos == vector<value_type>::end())
-			throw std::out_of_range(__func__);
+			throw std::out_of_range("iterator tuple_vector::at(const K &key)");
 		return pos;
 	}
-
 	inline const_iterator at(const K &key) const
 	{
 		auto pos = find(key);
 		if (pos == vector<value_type>::end())
-			throw std::out_of_range(__func__);
+			throw std::out_of_range("const_iterator tuple_vector::at(const K &key) const");
 		return pos;
 	}
-
-	/*
-	inline const value_type &last() const {
-		return *vector<value_type>::crbegin();
-	}
-
-	inline const value_type &first() const {
-		return *vector<value_type>::cbegin();
-	}
-	*/
-
-	/**
-	 * \brief Gets the last key/value pair from the timeseries.
-	 * \return Mutable reference to the last key/value pair of the timeseries.
-	 */
-	/*
-	inline value_type &last() {
-		return *vector<value_type>::rbegin();
-	}
-
-	inline value_type &first() {
-		return *vector<value_type>::begin();
-	}
-	*/
-
-	/*
-	// const std::pair<K, V> &get(unsigned idx) const { assert(idx < m_vec->size()); return m_vec[idx]; }
-	inline const value_type &get(unsigned idx) const {
-		return at(idx);
-	}
-	inline value_type &get(unsigned idx) {
-		return at(idx);
-	}
-	*/
 
 	/**
 	 * \brief Const array access operator.
 	 * \param idx Index into the timeseries data.
 	 * \return Const reference to the appropriate timeseries value.
 	 */
-	// const V &operator[](unsigned idx) const { assert(idx < m_vec->size()); return m_vec[idx].second; }
+	/*
 	inline const V &operator[](unsigned idx) const {
 		return vector<value_type>::operator [](idx).second;
 	}
-	/*
 	*/
 
 	/**
@@ -148,64 +200,9 @@ public:
 	 * \param idx Index into the timeseries data.
 	 * \return Mutable reference to the appropriate timeseries value.
 	 */
-	// V &operator[](unsigned idx)	{ assert(idx < m_vec->size()); return m_vec[idx].second; }
+	/*
 	inline V &operator[](unsigned idx)	{
 		return vector<value_type>::operator [](idx).second;
-	}
-	/*
-	*/
-
-	/**
-	 * \brief Gets a value given a key.
-	 * \param key The key whose value should be found.
-	 * \return Const reference to the appropriate timeseries value.
-	 * \todo Do we really need an assert when accessing the data via the key?!?
-	 */
-	/*
-	inline const V &get(const K &key) const {
-		auto pos = lower_bound(key);
-		assert(pos != vector<value_type>::cend());
-		return pos->second;
-	}
-
-	inline V &get(const K &key) {
-		auto pos = lower_bound(key);
-		assert(pos != vector<value_type>::end());
-		return pos->second;
-	}
-	*/
-
-	/*
-	inline const_iterator get_iter(const K &key) const {
-		const_iterator pos = lower_bound(key);
-		assert(pos != vector<value_type>::cend());
-		return pos;
-	}
-
-	inline iterator get_iter(const K &key) {
-		iterator pos = lower_bound(key);
-		assert(pos != vector<value_type>::end());
-		return pos;
-	}
-	*/
-
-	/* is this still needed?
-	size_t erase(const K &key, size_t idx) {
-		assert(idx < m_vec->size());
-		m_vec->erase(m_vec->begin() + idx);
-		return m_map->erase(key);
-	}
-	*/
-
-	/*
-	inline void set(const K &key, const V &val) {
-		auto pos = lower_bound(key);
-		assert(pos != vector<value_type>::end());
-		pos->second = val;
-	}
-
-	inline void set(size_t idx, const V &val) {
-		vector<value_type>::operator [](idx) = val;
 	}
 	*/
 
@@ -223,39 +220,6 @@ public:
 	 */
 	inline V &operator[](const K &key)	{ return lower_bound(key)->second; }
 
-	/**
-	 * \brief Get all values from this timeseries.
-	 * \retval v Vector that will contain all values from this timeseries.
-	 */
-	/*
-	void values(std::vector<V> &v) const {
-		v.clear();
-		v.reserve(vector<value_type>::size());
-		for (auto &i : *this) {
-			v.emplace_back(i.second);
-		}
-	}
-	*/
-
-	/**
-	 * \brief Comparison operator.
-	 * \param op The timeseries which should be compared.
-	 * \return True if the size() < op.size(), false otherwise.
-	 */
-	/*
-	inline bool operator<(const tuple_vector<K, V> &op) const
-	{
-		return vector<value_type>::size() < op.vector<value_type>::size();
-	}
-
-	tuple_vector<K, V> &operator=(const tuple_vector<K, V> &op) {
-		if (this != &op) {
-			vector<value_type>::operator =(op);
-		}
-		return *this;
-	}
-	*/
-
 	/*
 	friend std::ostream &operator<< (std::ostream &os, const tuple_vector<K, V> &t) {
 		auto pos = vector<value_type>::crbegin();
@@ -267,60 +231,6 @@ public:
 	}
 	*/
 
-
-	/**
-	 * \brief Checks the existence of a given key.
-	 * \param key The key to be checked.
-	 * \return True or false, depending if the key has been found or not.
-	 */
-	/*
-	inline bool exists(const K &key) const
-	{
-		if (debug) std::cout << "tuple_vector::exists('" << key << "')" << std::endl;
-		const_iterator pos = lower_bound(key);
-		// typename std::map<datetime, double>::const_iterator pos = m_map->find(key);
-		if (pos == vector<value_type>::cend()) {
-			if (debug) std::cout << "pos == m_map.end() !!!" << std::endl;
-			return false;
-		}
-		if (pos->first == key) {
-			if (debug) std::cout << "pos->first == key !!!" << std::endl;
-			return true;
-		}
-		return false;
-	}
-	*/
-
-
-	// insert element at end
-	inline void emplace_back(const pair<K,V> &&p)
-	{
-		vector<value_type>::emplace_back(p);
-		m_recompute_range = true;
-	}
-	inline void emplace_back(const pair<K,V> &p)
-	{
-		vector<value_type>::emplace_back(p);
-		m_recompute_range = true;
-	}
-	size_type erase(const K &key)
-	{
-		auto pos = find(key);
-		m_recompute_range = true;
-		vector<value_type>::erase(pos);
-		return 1;
-	}
-	iterator erase(const_iterator pos)
-	{
-		m_recompute_range = true;
-		return vector<value_type>::erase(pos);
-	}
-	iterator erase(const_iterator start, const_iterator end)
-	{
-		m_recompute_range = true;
-		return vector<value_type>::erase(start, end);
-	}
-
 	inline iterator lower_bound(const K &key)
 	{
 		return iterator(const_cast<value_type *>(_lower_bound(key)));
@@ -329,7 +239,7 @@ public:
 	inline const_iterator lower_bound(const K &key) const
 	{
 		return const_iterator(_lower_bound(key));
-	}	// lower_bound()
+	}
 
 	// reset housekeeping variables
 	void reset()
@@ -351,58 +261,6 @@ public:
 	{
 		return const_iterator(_find(key));
 	}
-
-	/*
-	iterator find(const K &key)
-	{
-		value_type *rc = nullptr;
-
-		if (m_recompute_range) {
-			m_size = vector<value_type>::size();
-			m_total_range = key<>()(vector<value_type>::back().first) - key<>()(vector<value_type>::front().first);
-			m_element_range = m_total_range / (m_size-1);
-			m_offset = 0;
-			m_recompute_range = false;
-			++m_recompute;
-		}
-
-		size_t idx = (key<>()(key) - (key<>()(vector<value_type>::front().first)) / m_element_range + m_offset;
-
-		if (idx >= 0 && idx < m_size) {
-			// cout << "key could be at index position " << idx << " (offset " << offset << ")" << endl;
-			rc = &this->operator[](idx);
-		} else {
-			// idx is out of bounds, start from the beginning
-			// rc = &this->operator[](0);
-			if (idx > m_size)
-				rc = &vector<value_type>::back();
-			else
-				rc = &vector<value_type>::front();
-			m_offset = 0;
-			++m_outofbound;
-		}
-		if (rc->first == key) {
-			++m_hits;
-			return iterator(rc);
-		}
-
-		// offset needs re-synch
-		int old_offset = m_offset;
-		if (rc->first > key)
-			for (; rc->first > key; --rc, --m_offset);
-		else
-			for (; rc->first < key; ++rc, ++m_offset);
-		m_resync += abs(m_offset - old_offset);
-
-		// cout << key << ": off by: " << (m_offset-old_offset) << endl;
-
-		if (rc->first == key) {
-			return iterator(rc);
-		}
-
-		return vector<value_type>::end();
-	}	// find()
-	*/
 
 	int resync() const { return m_resync; }
 	int recompute() const { return m_recompute; }
