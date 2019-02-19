0637e774a5bbd5103cc367cccdd21df99b7bbe18
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2019-02-19 02:56:23 +0000

    moved common code to include files; added custom result writing function for later plotting with R

--- perf.cc
+++ perf.cc
@@ -4,201 +4,105 @@
 #include <vector>
 #include <map>
 
-#include <boost/date_time/posix_time/posix_time_types.hpp>
-#include <boost/date_time.hpp>
-
 // please also clone my cppbench library ( https://github.com/sinisa-susnjar/cppbench.git )
 #include "../cppbench/cppbench.h"
 
 #include "tuple_vector.h"
+#include "common.h"
 
-using namespace boost::posix_time;
-using namespace boost::gregorian;
 using namespace std;
 
-/// dummy helper class to make boost::posix_time::ptime work with run_test()
-class my_ptime : public ptime {
-public:
-	my_ptime(int n = 0) : ptime(boost::gregorian::date(1970, Jan, 1)) { }
-	my_ptime &operator++(int) {
-		*this += seconds(1);
-		return *this;
-	}
-};
-
-/// sample key functor specialisation for ptime
-template<> double key<my_ptime>::operator()(const my_ptime &x) const { return (x-ptime(date(1900, Jan, 1))).total_milliseconds(); }
-/// sample key functor specialisation for time_t
-template<> double key<time_t>::operator()(const time_t &x) const { return x; }
+/**
+ * \brief Specialised output function to generate data that will later plotted by R.
+ * \param os Where to write the test results.
+ * \param result A multimap with the runtimes of all tests obtained by a call to time().
+ * \param name Test group name.
+ * \param sz Container size.
+ * \param delim What delimiter character to use, default is tab.
+ * \param prec Numerical precision for floating value, default is 5.
+ * \tparam T An optional template parameter used to cast to the desired time resolution.
+ */
+template<typename T = chrono::microseconds>
+void Rdata(ostream &os, const cppbench::runtime_result &result, const string &name, int sz, char delim = '\t', int prec = 5) {
+	double conv = chrono::high_resolution_clock::period::den / T::period::den;
+	for (const auto &a : result) 
+		os << name << delim << sz << delim << a.second.name << delim
+			<< chrono::duration_cast<chrono::microseconds>(a.first).count() << delim
+			<< fixed << setprecision(prec) << (a.second.min/conv) << delim
+			<< fixed << setprecision(prec) << (a.second.max/conv) << delim
+			<< fixed << setprecision(prec) << (a.second.avg/conv) << delim
+			<< fixed << setprecision(prec) << (a.second.var/(conv*conv)) << delim
+			<< fixed << setprecision(prec) << (a.second.dev/conv)
+			<< endl;
+}
 
 /**
  * \brief run_tests template function for running same tests for different types of datetime types
+ * \param n_tests number of runs for each test
+ * \param start_sz container start size
+ * \param end_sz container max size
+ * \param sz_step container size increment
  * \tparam K a datetime type supporting initialisation from int and post-increment
  */
 template<typename K>
-void run_tests()
+void run_tests(ofstream &&ofs, int n_tests, int start_sz, int end_sz, int sz_step)
 {
-	vector<pair<K, double>> ts;
+	ofs << "test" << '\t' << "size" << '\t' << "container" << '\t'
+		<< "runtime" << '\t' << "min" << '\t' << "max" << '\t'
+		<< "avg" << '\t' << "var" << '\t' << "dev" << endl;
+	for (int sz = start_sz; sz <= end_sz; sz += sz_step) {
+		vector<pair<K, double>> ts;
+		K dt = 0;
 
-	constexpr size_t SZ = 5000000;
-	constexpr size_t N = 1;
+		// create dummy timeseries with strictly increasing time values
+		for (size_t n = 0; n < sz; n++, dt++) {
+			ts.emplace_back(make_pair(dt, 3.1415926));
+			// if (n%23 == 0) dt++; // uncomment to add some "sparsity" - this should decrease the access performance (a little)
+		}
 
-	K dt = 0;
+		vector<pair<K, double>> vec;
+		tuple_vector<K, double> tv;
+		map<K, double> map;
 
-	// create dummy timeseries with strictly increasing time values
-	for (size_t n = 0; n < SZ; n++, dt++) {
-		ts.emplace_back(make_pair(dt, 3.1415926));
-		// if (n%23 == 0) dt++; // uncomment to add some "sparsity" - this should decrease the access performance (a little)
+		{	// emplace performance
+			cout << "emplace with size " << sz << endl;
+			#include "tests/emplace_test.h"
+			Rdata(ofs, rt, "emplace", sz);
+		}
+
+		{	// operator[] performance
+			cout << "array with size " << sz << endl;
+			#include "tests/array_test.h"
+			Rdata(ofs, rt, "array", sz);
+		}
+
+		{	// iterator performance
+			cout << "iterator with size " << sz << endl;
+			#include "tests/iterator_test.h"
+			Rdata(ofs, rt, "iterator", sz);
+		}
+
+		{	// find() performance
+			cout << "find with size " << sz << endl;
+			#include "tests/find_test.h"
+			Rdata(ofs, rt, "find", sz);
+		}
+
+		{	// lower_bound() performance
+			cout << "lower_bound with size " << sz << endl;
+			#include "tests/lower_bound_test.h"
+			Rdata(ofs, rt, "lower_bound", sz);
+		}
 	}
-
-	tuple_vector<K, double> tv;
-	map<K, double> map;
-	vector<K> vec;
-
-	// emplace performance
-	auto rt = cppbench::time(N, {
-		 { "vector", [ts,&vec]() {
-			vec.reserve(ts.size());
-			for (const auto &it : ts)
-				vec.emplace_back(it.first);
-		}},
-		{ "map",	[ts,&map]() {
-			for (const auto &it : ts)
-				map.emplace_hint(map.end(), it.first, it.second);
-		
-		}},
-		{ "tuple",	[ts,&tv]() {
-			tv.reserve(ts.size());
-			for (const auto &it : ts)
-				tv.emplace_back(it);
-		}}
-	});
-
-	cout << "emplace()" << endl;
-	cppbench::print( cppbench::compare(rt) );
-
-	// operator[] performance
-	rt = cppbench::time(N, {
-		 { "vector", [ts,vec]() {
-			size_t i = 0;
-			for (const auto &it : ts)
-				if (vec[i++] != it.first)
-					abort();
-		}},
-		{ "map",	[ts,&map]() {
-			for (const auto &it : ts)
-				if (map[it.first] != it.second)
-					abort();
-		}},
-		{ "tuple",	[ts,tv]() {
-			size_t i = 0;
-			for (const auto &it : ts)
-				if (tv[i++].second != it.second)
-					abort();
-		}}
-	});
-
-	cout << endl << "operator []" << endl;
-	cppbench::print( cppbench::compare(rt) );
-
-	// iterator performance
-	rt = cppbench::time(N, {
-		 { "vector", [ts,vec]() {
-			auto it = ts.begin();
-			for (auto &x : vec) {
-				if (x != it->first)
-					abort();
-				++it;
-			}
-		}},
-		{ "map",	[ts,map]() {
-			auto x = map.begin();
-			auto i = ts.begin();
-			for (; i != ts.end(); ++i, ++x) {
-				if (x->first != i->first)
-					abort();
-			}
-		
-		}},
-		{ "tuple",	[ts,tv]() {
-			auto x = tv.begin();
-			auto i = ts.begin();
-			for (; i != ts.end(); ++i, ++x) {
-				if (x->first != i->first)
-					abort();
-			}
-		}}
-	});
-
-	cout << endl << "iterator" << endl;
-	cppbench::print( cppbench::compare(rt) );
-
-	// find() performance
-	rt = cppbench::time(N, {
-		{ "map",	[ts,map]() {
-			for (auto &i : ts) {
-				auto it = map.find(i.first);
-				if (it->first != i.first)
-					abort();
-			}
-		}},
-		{ "tuple",	[ts,&tv]() {
-			for (auto &i : ts) {
-				auto it = tv.find(i.first);
-				if (it == tv.end())
-					abort();
-				if ((*it).first != i.first)
-					abort();
-			}
-		}}
-	});
-
-	cout << endl << "find()" << endl;
-	cppbench::print( cppbench::compare(rt) );
-
-	// lower_bound() performance
-	rt = cppbench::time(N, {
-		{ "map",	[ts,map]() {
-			K dt;
-			K l = ts.crbegin()->first;
-			for (auto &i : ts) {
-				dt = i.first;
-				dt++;
-				if (dt > l)
-					break;
-				auto it = map.lower_bound(dt);
-				if (it == map.end())
-					abort();
-				if ((*it).first < dt)
-					abort();
-			}
-		}},
-		{ "tuple",	[ts,&tv]() {
-			K dt;
-			K l = ts.crbegin()->first;
-			for (auto &i : ts) {
-				dt = i.first;
-				dt++;
-				if (dt > l)
-					break;
-				auto it = tv.lower_bound(dt);
-				if (it == tv.end())
-					abort();
-				if ((*it).first < dt)
-					abort();
-			}
-		}}
-	});
-
-	cout << endl << "lower_bound()" << endl;
-	cppbench::print( cppbench::compare(rt) );
 }
 
-int
-main()
+int main()
 {
+	// run 1000x tests, using container sizes from 1000 to 20000 with step size 1000
+
 	cout << "RUNNING TESTS FOR time_t" << endl;
-	run_tests<time_t>();
+	run_tests<time_t>(ofstream("timet.txt"), 1000, 1000, 20000, 1000);
+
 	cout << endl << "RUNNING TESTS FOR boost::posix_time::ptime" << endl;
-	run_tests<my_ptime>();
+	run_tests<my_ptime>(ofstream("ptime.txt"), 1000, 1000, 20000, 1000);
 }
