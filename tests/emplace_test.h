d4180236584a9cf1003fa6a8161ff52349d3a5d4
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2019-02-19 05:03:47 +0000

    just some tests

--- tests/emplace_test.h
+++ tests/emplace_test.h
@@ -1,6 +1,12 @@
 		auto rt = cppbench::time(n_tests, {
-			 { "vector", [ts,&vec]() {
-			 	vec.clear();
+			{ "tuple",	[ts,&tv]() {
+				tv.clear();
+				tv.reserve(ts.size());
+				for (const auto &it : ts)
+					tv.emplace_back(it);
+			}},
+			{ "vector", [ts,&vec]() {
+				vec.clear();
 				vec.reserve(ts.size());
 				for (const auto &it : ts)
 					vec.emplace_back(it);
@@ -10,11 +16,5 @@
 				for (const auto &it : ts)
 					map.emplace_hint(map.end(), it.first, it.second);
 			
-			}},
-			{ "tuple",	[ts,&tv]() {
-				tv.clear();
-				tv.reserve(ts.size());
-				for (const auto &it : ts)
-					tv.emplace_back(it);
 			}}
 		});
