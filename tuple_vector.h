1d9f1e4202f03bb5392c0ea64698b37e16dbd915
Siniša Šušnjar <sinisa.susnjar@gmail.com>
2021-12-24 19:55:06 +0000

    added more comments

--- tuple_vector.h
+++ tuple_vector.h
@@ -214,37 +214,53 @@ protected:
 	{
 		const value_type *rc = nullptr;
 
+		// need to keep internal house-keeping variables in sync?
 		if (m_recompute_range) {
+			// get current size of vector
 			m_size = std::vector<value_type>::size();
+			// calculate the "total range" or time difference between front and back elements
 			m_total_range = ::key<K>()(std::vector<value_type>::back().first) - ::key<K>()(std::vector<value_type>::front().first);
+			// how much "time" does one element occupy
 			m_element_range = m_total_range / (m_size-1);
 			m_offset = 0;
-			m_recompute_range = false;
+			// get pointer to first element - this serves as the lower bound when searching for key
 			m_front = &std::vector<value_type>::front();
+			// get pointer to last element - this serves as the upper bound when searching for key
 			m_back = &std::vector<value_type>::back();
+			// keep a count of how many times we were called
 			++m_recompute;
+			// done updating internal house-keeping
+			m_recompute_range = false;
 		}
 
+		// if there is no data, we are already done
 		if (m_size == 0)
 			return std::vector<value_type>::_M_impl._M_finish;
 
+		// compute the initial guess where we could find the searched key by interpolating
 		size_t idx = (::key<K>()(key) - ::key<K>()(std::vector<value_type>::front().first)) / m_element_range + m_offset;
 
+		// check bounds first
 		if (idx >= 0 && idx < m_size) {
+			// ok, we are within bounds - get element
 			rc = &std::vector<value_type>::operator[](idx);
 		} else {
+			// out-of-bounds, get either front or back element, depending on the index
 			if (idx > m_size)
 				rc = &std::vector<value_type>::back();
 			else
 				rc = &std::vector<value_type>::front();
 			m_offset = 0;
+			// maintain an out-of-bounds counter for information purposes
 			++m_outofbound;
 		}
+		// if we already found our key, return it
 		if (rc->first == key) {
 			++m_hits;
 			return rc;
 		}
 		const value_type *old_rc = rc;
+		// perform a search backward or forward, depending on were we landed
 		if (rc->first > key)
 			for (; rc->first > key && rc > m_front; --rc);
 		else
@@ -256,9 +272,11 @@ protected:
 		m_avg_diff /= m_resync;
 		m_offset = (double)m_offset + m_avg_diff + .5;
 
+		// did we find the sought key? return it
 		if (rc->first == key)
 			return rc;
 
+		// not found? return end()
 		return std::vector<value_type>::_M_impl._M_finish;
 	}
 
@@ -290,6 +308,7 @@ protected:
 			else
 				rc = &std::vector<value_type>::front();
 			m_offset = 0;
+			// maintain an out-of-bounds counter for information purposes
 			++m_outofbound;
 		}
 		const value_type *old_rc = rc;
@@ -315,18 +334,30 @@ protected:
 	}
 
 private:
+	/// number of elements currently in tuple_vector
 	mutable size_t m_size = 0;
+	/// "total range" or time difference between first and last element in tuple_vector
 	mutable double m_total_range = 0;
+	/// how much "time" does one element occupy within tuple_vector (adjusted)
 	mutable double m_element_range = 0;
+	/// offset added to the initial interpolation guess to compensate for "time" gaps
 	mutable int m_offset = 0;
-	mutable bool m_recompute_range = false;
-	mutable int m_resync = 0;
-	mutable int m_recompute = 0;
-	mutable int m_outofbound = 0;
-	mutable int m_hits = 0;
-	mutable const value_type *m_front = nullptr;
-	mutable const value_type *m_back = nullptr;
+	/// running average difference between key we guessed at first try and key we landed on after resync
 	mutable double m_avg_diff = 0;
+	/// flag set in modifying methods in order to trigger housekeeping in find() and lower_bound()
+	mutable bool m_recompute_range = false;
+	/// count how often a key was found on the first try
+	mutable int m_hits = 0;
+	/// how often a resync was necessary, i.e. when a key was not found on the first try
+	mutable int m_resync = 0;
+	/// how often the internal housekeeping code was called
+	mutable int m_recompute = 0;
+	/// how often a searched triggered an out-of-bounds condition
+	mutable int m_outofbound = 0;
+	/// pointer to the first element in this tuple_vector
+	mutable const value_type *m_front = nullptr;
+	/// pointer to the last element in this tuple_vector
+	mutable const value_type *m_back = nullptr;
 };
 
 #endif /* __tuple_vector_h */
